c4ffb08ac27d492d3c65ee6f406ce93005dc3123
xanderyui <alexander.galdones@gmail.com>
2015-10-21 00:38:44 +0800

    Init commit test project

---
+++ app/Http/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('start');
+});
+
+
+Route::match(['get','post'], 'd/news', function() {
+		return file_get_contents('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=8&q=http%3A%2F%2Fnews.google.com%2Fnews%3Foutput%3Drss&rsz=8');
+});
+
+
